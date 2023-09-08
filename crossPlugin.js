fe0ce02a290abe0bf5138bebc09c7094a655b4fe
ozonar <7EvilCyberBears>
2023-09-08 23:29:37 +0700

    Initial commit

---
+++ crossPlugin.js
@@ -0,0 +1,210 @@
+// ==UserScript==
+// @name         Cross plugin
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to close the world!
+// @author       Anarhistov
+// @match        https://www.ozon.ru/*
+// @match        https://www.dns-shop.ru/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+class CrossPlugin {
+    static run(config) {
+        let cards = this.findCards(config.elementSelector);
+        if (cards.length === 0) {
+            return;
+        }
+
+        console.log('::', 'Start cross plugin. Find elements:', cards.length);
+        this.paintButtons(cards, config.buttonConfig);
+        this.checkRemoveCards(cards);
+        this.addListeners(cards, config.elementSelector, config.linkSelector);
+    }
+
+    static findCards(selector) {
+        return document.querySelectorAll(selector + ':not(.element-with-close)');
+    }
+
+    static paintButtons(items, buttonConfig) {
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i];
+            this.paintButton(item, buttonConfig);
+        }
+    }
+
+    static addListeners(items, selector, valueSelector) {
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i];
+
+            item.querySelector('.close-hide-element').addEventListener('click', function (e) {
+                e.preventDefault();
+                CrossPlugin.removeEl(e.target.parentNode);
+                Storage.add(item.querySelector(valueSelector).href)
+            });
+        }
+    }
+
+
+    static checkRemoveCards(items) {
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i];
+            if (this.checkElementNeedRemove(item)) {
+                CrossPlugin.removeEl(item);
+            }
+        }
+    }
+
+    static checkElementNeedRemove(item) {
+        let a = item.querySelector('a');
+        if (a) {
+            let href = Storage.clear(a.href);
+            if (Storage.items.includes(href)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static paintButton(item, buttonConfig) {
+        let div = document.createElement('div');
+
+        div.style.width = '30px';
+        div.style.height = '30px';
+        div.style.backgroundColor = '#ff9999';
+        div.style.position = 'absolute';
+        div.style.right = buttonConfig.right ?? 15 + 'px';
+        div.style.bottom = buttonConfig.bottom + 'px';
+        div.style.borderRadius = '9px';
+        div.style.cursor='pointer';
+
+        div.classList.add('close-hide-element');
+        item.appendChild(div);
+
+        item.classList.add('element-with-close');
+    }
+
+    static removeEl(el) {
+        el.style.opacity = '0.1';
+
+        let div = document.createElement('div');
+
+        div.style.width = '100%';
+        div.style.height = '100%';
+        div.style.backgroundColor = '#ffffff';
+        div.style.position = 'absolute';
+        div.style.opacity = '0';
+        div.style.zIndex = '100000';
+        div.classList.add('hide-card');
+
+        el.appendChild(div);
+
+        div.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            Storage.delete(e.target.parentElement.querySelector('a').href);
+            e.target.parentElement.style.opacity = '1';
+            e.target.remove();
+
+        });
+    }
+}
+
+class Storage {
+    static items = [];
+
+    static add(item) {
+        let clearItem = Storage.clear(item);
+
+        if (!Storage.isInArray(clearItem)) {
+            Storage.items.push(clearItem);
+            Storage.save();
+        }
+    }
+
+    static delete(item) {
+        let clearItem = Storage.clear(item);
+
+        let val = Storage.items.indexOf(clearItem);
+        if (val) {
+            Storage.items.splice(val, 1);
+            Storage.save();
+        }
+    }
+
+    static save() {
+        localStorage.setItem('cross-plugin', JSON.stringify(Storage.items));
+    }
+
+    static isInArray(href) {
+        return Storage.items.includes(href);
+    }
+
+    static clear(item) {
+        let url = new URL(item);
+        return url.pathname;
+    }
+
+    static init() {
+        let a = localStorage.getItem('cross-plugin');
+        if (a) {
+            Storage.items = JSON.parse(a);
+        } else {
+            Storage.items = [];
+        }
+    }
+}
+
+class HostConfig {
+    static list = {
+        'www.ozon.ru': {
+            'elementSelector': '.widget-search-result-container > div > div',
+            'linkSelector': 'a',
+            'buttonConfig': {
+                'bottom' : 71,
+            }
+        },
+        'www.dns-shop.ru': {
+            'elementSelector': '.catalog-product',
+            'linkSelector': 'a',
+            'buttonConfig': {
+                'bottom' : 15,
+            }
+        }
+    };
+
+    static get(host) {
+        return this.list[host];
+    }
+}
+
+let config = HostConfig.get(document.location.host);
+Storage.init();
+
+load();
+
+function load() {
+    setInterval(function () { CrossPlugin.run(config); }, 400);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
